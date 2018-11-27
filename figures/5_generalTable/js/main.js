68e7bf4b6e01d64ce98b6df359384a03d1987599
bernatferragut <elbernat@gmail.com>
2018-11-27 12:11:47 -0500

    TAULA GENERALIS V.1.0

--- figures/5_generalTable/js/main.js
+++ figures/5_generalTable/js/main.js
@@ -1,12 +1,14 @@
-// THIRD FIGURE v.1.0 - LLULLIAN FIGURES by BERNAT FERRGUT
+// GERNERAL TABLE v.1.0 - LLULLIAN FIGURES by BERNAT FERRGUT
 
 // Vars
 let w = window.innerWidth, h = window.innerHeight;
 let diameter_2 = 535
-const LETTERS = ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K']
+// const LETTERS = ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K']
 const COLORS = ['rgb(255,255,255)', 'rgb(0,0,0)', 'rgb(255,0,0)', 'rgb(0,255,43)', 'rgb(255,255,0)']
 let multiplier = 1, pairs = [], letterPairs
-let chamberW = 63, chamberH = 42, font = 10, scaleFactor = 1.3// the proportions of the king's chanmber in Giza Pyramid
+let chamberW = 63, chamberH = 21, font = 10, scaleFactor = 1.3// the proportions of the king's chanmber in Giza Pyramid
+const LETTERS = ['B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K']
+let TRIOS;
 
 
 // P5JS functions
@@ -18,7 +20,7 @@ function setup() {
 }
 
 function draw() {
-  scale(scaleFactor)
+  // scale(scaleFactor)
   halfMatrix()
 }
 
@@ -29,44 +31,171 @@ const halfMatrix = () => {
   stroke(COLORS[3])
   strokeWeight(0.5)
 
-  // creatig PAIRS
-  pairs = pairwiseA(LETTERS)
-  console.table(pairs)
-  
+  // creatig TRIOS by HAND
+  TRIOS = [['B','C','D','T'],// 1
+          ['B','C','T','B'],
+          ['B','C','T','C'],
+          ['B','C','T','D'],
+          ['B','D','T','B'],
+          ['B','D','T','C'],
+          ['B','D','T','D'],
+          ['B','T','B','C'],
+          ['B','T','B','D'],
+          ['B','T','C','D'],
+          ['C','D','T','B'],
+          ['C','D','T','C'],
+          ['C','D','T','D'],
+          ['C','T','B','C'],
+          ['C','T','B','D'],
+          ['C','T','C','D'],
+          ['D','T','B','C'],
+          ['D','T','B','D'],
+          ['D','T','C','D'],
+          ['T','B','C','D'],
+          ['C','D','E','T'], // 2
+          ['C','D','T','C'],
+          ['C','D','T','D'],
+          ['C','D','T','E'],
+          ['C','E','T','C'],
+          ['C','E','T','D'],
+          ['C','E','T','E'],
+          ['C','T','C','D'],
+          ['C','T','C','E'],
+          ['C','T','D','E'],
+          ['D','E','T','C'],
+          ['D','E','T','D'],
+          ['D','E','T','E'],
+          ['D','T','C','D'],
+          ['D','T','C','E'],
+          ['D','T','D','E'],
+          ['E','T','C','D'],
+          ['E','T','C','E'],
+          ['E','T','D','E'],
+          ['T','C','D','E'],
+          ['D','E','F','T'], // 3
+          ['D','E','T','D'],
+          ['D','E','T','E'],
+          ['D','E','T','F'],
+          ['D','F','T','D'],
+          ['D','F','T','E'],
+          ['D','F','T','F'],
+          ['D','T','D','E'],
+          ['D','T','D','F'],
+          ['D','T','E','F'],
+          ['E','F','T','D'],
+          ['E','F','T','E'],
+          ['E','F','T','F'],
+          ['E','T','D','E'],
+          ['E','T','D','F'],
+          ['E','T','E','F'],
+          ['F','T','D','E'],
+          ['F','T','D','F'],
+          ['F','T','E','F'],
+          ['T','D','E','F'],
+          ['E','F','G','T'], // 4
+          ['E','F','T','E'],
+          ['E','F','T','F'],
+          ['E','F','T','G'],
+          ['E','G','T','E'],
+          ['E','G','T','F'],
+          ['E','G','T','G'],
+          ['E','T','E','F'],
+          ['E','T','E','G'],
+          ['E','T','F','G'],
+          ['F','G','T','E'],
+          ['F','G','T','F'],
+          ['F','G','T','G'],
+          ['F','T','E','F'],
+          ['F','T','E','G'],
+          ['F','T','F','G'],
+          ['G','T','G','F'],
+          ['G','T','E','G'],
+          ['G','T','F','G'],
+          ['T','E','F','G'],
+          ['F','G','H','T'], // 5
+          ['F','G','T','F'],
+          ['F','G','T','G'],
+          ['F','G','T','H'],
+          ['F','H','T','F'],
+          ['F','H','T','H'],
+          ['F','H','T','H'],
+          ['F','T','F','G'],
+          ['F','T','F','H'],
+          ['F','T','G','H'],
+          ['G','H','T','F'],
+          ['G','H','T','G'],
+          ['G','H','T','H'],
+          ['G','T','F','G'],
+          ['G','T','F','H'],
+          ['G','T','G','H'],
+          ['H','T','H','G'],
+          ['H','T','F','H'],
+          ['H','T','G','H'],
+          ['T','F','G','H'],
+          ['G','H','I','T'], // 6
+          ['G','H','T','G'],
+          ['G','H','T','H'],
+          ['G','H','T','I'],
+          ['G','I','T','G'],
+          ['G','I','T','I'],
+          ['G','I','T','I'],
+          ['G','T','G','H'],
+          ['G','T','G','I'],
+          ['G','T','H','I'],
+          ['H','I','T','G'],
+          ['H','I','T','H'],
+          ['H','I','T','I'],
+          ['H','T','G','H'],
+          ['H','T','G','I'],
+          ['H','T','H','I'],
+          ['I','T','I','H'],
+          ['I','T','G','I'],
+          ['I','T','H','I'],
+          ['T','G','H','I'],
+          ['H','I','K','T'], // 7
+          ['H','I','T','H'],
+          ['H','I','T','I'],
+          ['H','I','T','K'],
+          ['H','K','T','H'],
+          ['H','K','T','K'],
+          ['H','K','T','K'],
+          ['H','T','H','I'],
+          ['H','T','H','K'],
+          ['H','T','I','K'],
+          ['I','K','T','H'],
+          ['I','K','T','I'],
+          ['I','K','T','K'],
+          ['I','T','H','I'],
+          ['I','T','H','K'],
+          ['I','T','I','K'],
+          ['K','T','K','I'],
+          ['K','T','H','K'],
+          ['K','T','I','K'],
+          ['T','H','I','K'],
+          ]
+
   // half matrix building
   let i = 0 // this index will allow us to go over the hole pairs list
-  for(let h = 0; h < 8; h++) {
-    for(let v = 0; v < 8 - h; v++) { // here we shrink the verical giving half matrix
+  for(let h = 0; h < 7; h++) {
+    for(let v = 0; v < 20; v++) { // here we shrink the verical giving half matrix
     push()
-      // Rectangle creation\
+      // Rectangle creation
       rect(h * chamberW, v * chamberH, chamberW, chamberH)
-      // Pairs creation
-      letterPairs = new Word(h * chamberW, v * chamberH, font, CENTER, pairs[i][0] + pairs[i][1])
-      letterPairs.build_regular(0)
+      // Trios caption
+      trios = new Word(h * chamberW, v * chamberH, font, CENTER, TRIOS[i][0] + TRIOS[i][1] + TRIOS[i][2] + TRIOS[i][3])
+      trios.build_regular(0)
       i++
     pop()
     }
   }
 }
 
-// COMBINATRONICS
-function pairwiseA(list) {
-  if (list.length < 2) { return []; }
-  var first = list[0],
-      rest  = list.slice(1),
-      pairs = rest.map(function (x) { return [first, x]; });
-  return pairs.concat(pairwiseA(rest));
-}
 
-function pairwiseB(list) {
-  var pairs = [];
-  list
-    .slice(0, list.length - 1)
-    .forEach(function (first, n) {
-      var tail = list.slice(n + 1, list.length);
-      tail.forEach(function (item) {
-        pairs.push([first, item])
-      });
-    })
-  return pairs;
-}
+
+
+ 
+
+
+
+
+
